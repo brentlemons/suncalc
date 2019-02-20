a46cbaa5df7f29bab2474d6123a8f12b452349d0
Brent Lemons <brent.lemons@embloybridge.com>
2019-02-20 09:45:34 -0600

    quick note

--- suncalc.js
+++ suncalc.js
@@ -25,7 +25,7 @@ var PI   = Math.PI,
 var dayMs = 1000 * 60 * 60 * 24,
     J1970 = 2440588,
     J2000 = 2451545;
-
+//going to change to return ms
 function toJulian(date) { return date.valueOf() / dayMs - 0.5 + J1970; }
 function fromJulian(j)  { return new Date((j + 0.5 - J1970) * dayMs); }
 function toDays(date)   { return toJulian(date) - J2000; }
